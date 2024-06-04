f97bfde9ba62b16b1692036c5138bd9db70dfba9
Vincenzo Padula <padvincenzo@gmail.com>
2024-06-04 18:34:56 +0200

    Fix author tag

--- settings.php
+++ settings.php
@@ -18,7 +18,7 @@
  * Add config page to admin menu.
  *
  * @package     local_nolej
- * @author      2024 Vincenzo Padula <vincenzo@oc-group.eu>
+ * @author      Vincenzo Padula <vincenzo@oc-group.eu>
  * @copyright   2024 OC Open Consulting SB Srl
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
